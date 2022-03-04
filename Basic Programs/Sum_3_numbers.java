8e4e1fba4396536ac893e259670047f011ff0d80
VICKY <vishujoshi21nov2000@gmail.com>
2022-03-04 19:37:07 +0530

    Added .gitignore

---
+++ Basic Programs/Sum_3_numbers.java
@@ -0,0 +1,10 @@
+// Add 3 Numbers
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = 20;
+        System.out.println(a + b + c);
+    }
+}
