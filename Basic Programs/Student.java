895cd21ae10e291128d50a58c15bfa7c97a9e301
VICKY <vishujoshi21nov2000@gmail.com>
2022-04-15 09:53:30 +0530

    update .gitignore

---
+++ Basic Programs/Student.java
@@ -0,0 +1,17 @@
+class Student
+{
+
+    int rollno;
+    String name;
+
+    Student(int roll,String name)
+    {
+        this.rollno = rollno;
+        this.name = name;
+    }
+
+    public static void main(String[] args)
+    {
+        Student obj = new Student(63,"KNOCKCAT");
+    }
+}
