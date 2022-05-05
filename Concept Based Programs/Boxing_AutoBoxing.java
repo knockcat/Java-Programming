e3515e4029d01b74e27526bd1fda228298ca3743
VICKY <vishujoshi21nov2000@gmail.com>
2022-05-05 07:49:16 +0530

    AutoBoxing & UnBoxing

---
+++ Concept Based Programs/Boxing_AutoBoxing.java
@@ -0,0 +1,19 @@
+
+public class Boxing_AutoBoxing {
+
+    public static void main(String[] args) {
+        int a = 100;
+
+        Integer aobj = a; // AutoBoxing
+
+        Float fobj = 69.9f;
+
+        float ind = fobj; // UnBoxing
+
+        System.out.println("a is an (primitive) int value : " + a);
+        System.out.println("a is converted to an object of its Integer Wrapper Class aobj: " + aobj);
+        System.out.println("fobj is an object of Float Wrapper class  : " + fobj);
+        System.out.println("fobj is coverted to (primitive) float value : " + ind);
+
+    }
+}
